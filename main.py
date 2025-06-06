6d21ff4f255a040ceaceebaa4f965f3d9276b366
Fereshteh Farahani <f.feizabadifarahani@gmail.com>
2025-06-06 11:22:38 +0200

    functions for reporting added

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from expense import Expense
 from storage import store_expense, load_expense
-from report import summarize_by_category
+from report import summarize_by_category,summarize_monthly_yearly, plot_expenses
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
-df = load_expense("expenses.csv")
-summary = summarize_by_category(df)
-print(summary)
+if __name__ == "__main__":
+    df = load_expense("expenses.csv")
+
+    total_spent = df['amount'].sum()
+    print(f"\n💸 Total spent: {total_spent:.2f}\n")
+
+    print("📊 Expenses by Category:\n")
+    print(summarize_by_category(df))
+
+    print("\n🗓️ Monthly and Yearly Summary:\n")
+    print(summarize_monthly_yearly(df))
+
+    plot_expenses(df)
--- report.py
+++ report.py
@@ -1,5 +1,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
+
 def summarize_by_category(df):
     return df.groupby('category')['amount'].sum().reset_index()
+
+
+def summarize_monthly_yearly(df):
+    df['date'] = pd.to_datetime(df['date'])
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+    return df.groupby(['year','month'])['amount'].sum().reset_index()
+
+def plot_expenses(df):
+    df.plot(x='date', y='amount',kind = 'hist', title='Expenses Over Time')
+    df.plot(x='category', y='amount', kind = 'hist', title='Expenses Over Time')
+    return plt.show()
